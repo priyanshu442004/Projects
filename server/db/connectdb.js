59286d3b6ea2ecd6c983733a3373ccc9e1f06cbe
Priyanshu-444 <priyanshusharma442005@gmail.com>
2024-08-20 15:45:19 +0530

    Update Vite configuration for Vercel deployment

--- server/db/connectdb.js
+++ server/db/connectdb.js
@@ -2,7 +2,7 @@
 import mongoose from 'mongoose'
 const connectdb=async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect("mongodb+srv://priyanshusharma442004:FgEz1mmbDlZ17Roy@cluster0.zon98.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log("mongo db connected")
     }
     catch(error){
